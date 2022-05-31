eb65507f324543e7e88f89a0040d54c13f457ea3
do2anjos <99214399+do2anjos@users.noreply.github.com>
2022-05-30 22:50:56 -0400

    Delete index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { funcionario } from "./models/funcionário"
-import { lider } from "./models/lider"
-
-console.log("oi, teste!")
-
-let chefe:lider = lider.getInstance
-
-let p1:funcionario = new funcionario('Matheus', 20, 'Ti')
-let p2:funcionario = new funcionario('Marcos', 21, 'dp')
